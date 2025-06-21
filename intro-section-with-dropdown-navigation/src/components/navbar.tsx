fd3d1c5a37e2d3c650991f52d14e2619557a0165
diego <hermani457@gmail.com>
2025-06-21 17:56:56 -0400

    feat: refactor Navbar component for improved structure and accessibility

--- intro-section-with-dropdown-navigation/src/components/navbar.tsx
+++ intro-section-with-dropdown-navigation/src/components/navbar.tsx
@@ -29,79 +29,65 @@ const Navbar = () => {
     <>
       <nav className="hidden lg:flex items-center gap-10 py-6 px-4">
         <img src={logo} alt="Logo" />
-        <menu className="flex items-center gap-6 text-lg">
-          <div className="relative group">
-            <li className="cursor-pointer hover:font-semibold text-gray-500 hover:text-black">
+        <ul className="flex items-center gap-6 text-lg">
+          <li className="relative group">
+            <button className="cursor-pointer hover:font-semibold text-gray-500 hover:text-black">
               <span>Features</span>
               <span className="ml-1">
                 <ChevronDown className="inline size-4 group-hover:hidden" />
                 <ChevronUp className="hidden size-4 group-hover:inline" />
               </span>
-            </li>
+            </button>
             <div className="absolute right-0 hidden w-40 py-4 bg-white rounded-xl text-black shadow-lg group-hover:flex group-hover:justify-center">
               <ul>
-                <div>
-                  <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
-                    <img src={todo} alt="Todo Icon" />
-                    Todo List
-                  </li>
-                </div>
-                <div>
-                  <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
-                    <img src={calendar} alt="Calendar Icon" />
-                    Calendar
-                  </li>
-                </div>
-                <div>
-                  <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
-                    <img src={reminders} alt="Reminders Icon" />
-                    Reminders
-                  </li>
-                </div>
-                <div>
-                  <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
-                    <img src={planning} alt="Planning Icon" />
-                    Planning
-                  </li>
-                </div>
+                <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
+                  <img src={todo} alt="Todo Icon" />
+                  Todo List
+                </li>
+                <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
+                  <img src={calendar} alt="Calendar Icon" />
+                  Calendar
+                </li>
+                <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
+                  <img src={reminders} alt="Reminders Icon" />
+                  Reminders
+                </li>
+                <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
+                  <img src={planning} alt="Planning Icon" />
+                  Planning
+                </li>
               </ul>
             </div>
-          </div>
-          <div className="relative group">
-            <li className="cursor-pointer hover:font-semibold text-gray-500 hover:text-black">
+          </li>
+          <li className="relative group">
+            <button className="cursor-pointer hover:font-semibold text-gray-500 hover:text-black">
               Company
               <span className="ml-1">
                 <ChevronDown className="inline size-4 group-hover:hidden" />
                 <ChevronUp className="hidden size-4 group-hover:inline" />
               </span>
-            </li>
+            </button>
             <div className="absolute right-0 hidden w-40 py-4 bg-white rounded-xl text-black shadow-lg group-hover:flex group-hover:justify-center">
               <ul>
-                <div>
-                  <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
-                    History
-                  </li>
-                </div>
-                <div>
-                  <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
-                    Our Team
-                  </li>
-                </div>
-                <div>
-                  <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
-                    Blog
-                  </li>
-                </div>
+                <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
+                  History
+                </li>
+                <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
+                  Our Team
+                </li>
+                <li className="flex items-center gap-2 px-6 py-2 hover:bg-gray-100 rounded cursor-pointer">
+                  Blog
+                </li>
               </ul>
             </div>
-          </div>
-          <li className="hover:font-semibold cursor-pointer text-gray-500 hover:text-black">
-            Careers
           </li>
           <li className="hover:font-semibold cursor-pointer text-gray-500 hover:text-black">
-            About
+            <button>Careers</button>
           </li>
-        </menu>
+          <li className="hover:font-semibold cursor-pointer text-gray-500 hover:text-black">
+            <button>About</button>
+          </li>
+        </ul>
         <div className="ml-auto">
           <button className="py-2 px-4 rounded-lg text-gray-500 font-semibold hover:text-black cursor-pointer">
             Login
