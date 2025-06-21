1bef344795fef38dc6a31ab06b1d45bb25c3beea
diego <hermani457@gmail.com>
2025-06-21 17:17:16 -0400

    feat: refactor Hero component layout for improved responsiveness and structure

--- intro-section-with-dropdown-navigation/src/components/hero.tsx
+++ intro-section-with-dropdown-navigation/src/components/hero.tsx
@@ -8,22 +8,24 @@ import meet from "../assets/client-meet.svg";
 const Hero = () => {
   return (
     <main className="container mx-auto py-8 lg:px-16 lg:py-16">
-      <section className="flex flex-col-reverse lg:flex-row lg:justify-between lg:items-center gap-8 lg:gap-16">
+      <section className="flex flex-col-reverse lg:grid lg:grid-cols-2 lg:gap-16 lg:items-end">
         {/* left content */}
-        <div className="flex-1 max-w-xl space-y-8 lg:space-y-12 mx-auto text-center lg:text-left">
-          <h1 className="text-4xl md:text-5xl lg:text-7xl xl:text-8xl font-bold text-gray-900 ">
-            Make remote work
-          </h1>
+        <div className="space-y-8 lg:space-y-12 text-center lg:text-left">
+          <div className="space-y-8 lg:space-y-12">
+            <h1 className="text-4xl md:text-5xl lg:text-7xl xl:text-8xl font-bold text-gray-900">
+              Make remote work
+            </h1>
 
-          <p className="text-gray-500 text-lg lg:text-xl leading-relaxed max-w-md mx-auto lg:mx-0">
-            Get your team in sync, no matter your location. Streamline
-            processes, create team rituals, and watch productivity soar.
-          </p>
+            <p className="text-gray-500 text-lg lg:text-xl leading-relaxed max-w-xs md:max-w-md mx-auto lg:mx-0">
+              Get your team in sync, no matter your location. Streamline
+              processes, create team rituals, and watch productivity soar.
+            </p>
 
-          <div className="pt-4">
-            <button className="bg-gray-900 text-white font-semibold cursor-pointer px-6 py-3 rounded-2xl hover:bg-white hover:text-gray-900 border-2 border-gray-900 transition-colors duration-200">
-              Learn more
-            </button>
+            <div className="pt-4">
+              <button className="bg-gray-900 text-white font-semibold cursor-pointer px-6 py-3 rounded-2xl hover:bg-white hover:text-gray-900 border-2 border-gray-900 transition-colors duration-200">
+                Learn more
+              </button>
+            </div>
           </div>
 
           <div className="pt-8 lg:pt-16">
@@ -53,7 +55,7 @@ const Hero = () => {
         </div>
 
         {/* right image */}
-        <div className="flex-1 max-w-md lg:max-w-lg mx-auto lg:mx-0">
+        <div className="max-w-md lg:max-w-lg mx-auto lg:mx-0 mb-5 lg:mb-0">
           <img
             src={heroImage}
             alt="Hero"
